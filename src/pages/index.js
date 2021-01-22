c3f40a2fc139092a76179bfa1d34df86832cb039
Jarren San Jose <sanjose.jarren@gmail.com>
2021-01-22 00:30:34 -0800

    add a little more about me to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,14 +12,14 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Nav />
     <h1>jarrensj</h1>
-    <p>I'm Jarren. I'm 26 years old. I am a software engineer and I currently live in the Bay Area.</p>
+    <p>I'm Jarren. I'm 26 years old. I am a software engineer and I currently live in the Bay Area. I love BLACKPINK.</p>
     <h2>Current</h2>
     <ul>
       <li>Trading (<a href="https://public.com/jarrensj" style={{textDecoration: 'none'}}>Public portfolio</a>).</li>
       <li>Hanging out with my dog, <a href="https://dogoargentino.dog" style={{textDecoration: 'none'}}>Bash</a>.</li>
       <li>Wearing mismatched socks.</li>
-      <li>Staying at home.</li>
       <li><a href="https://www.nbatopshot.com/r/jarrensj" style={{textDecoration: 'none'}}>nbatopshot</a></li>
+      <li>Staying at home.</li>
     </ul>
     {/*
     <h2>Featured Projects</h2>
